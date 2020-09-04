1f38663927e99393f3aea508dc5d546484797138
hugo miranda <hugo.miranda.souza@gmail.com>
2020-09-04 13:32:18 -0300

    adicionado avatares aos users + service de  upload

--- src/server.ts
+++ src/server.ts
@@ -3,10 +3,12 @@ import express from 'express';
 import routes from './routes';
 
 import './database';
+import uploadConfig from './config/upload';
 
 const server = express();
 
 server.use(express.json());
+server.use('/files', express.static(uploadConfig.directory));
 server.use(routes);
 
 server.listen(3333, () => {
