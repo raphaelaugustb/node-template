d6103d9aff204c5c2a517e4c9c573f57473635d0
raphaelaugustb <travassosraphael12@gmail.com>
2020-09-09 10:12:51 -0300

    Node Template

--- src/config/auth.ts
+++
@@ -1,6 +0,0 @@
-export default {
-  jwt: {
-    secret: `;u2m"j;y!2:UQvkV)8%A(~PP-s?fk@`,
-    expiresIn: '1d',
-  },
-};
--- src/server.ts
+++ src/server.ts
@@ -3,12 +3,10 @@ import express from 'express';
 import routes from './routes';
 
 import './database';
-import uploadConfig from './config/upload';
 
 const server = express();
 
 server.use(express.json());
-server.use('/files', express.static(uploadConfig.directory));
 server.use(routes);
 
 server.listen(3333, () => {
